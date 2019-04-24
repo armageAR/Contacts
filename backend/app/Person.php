71699981565135d8285746bf66d79582138b4e4e
armage <armage@gmail.com>
2019-04-24 19:20:54 -0300

    added age function

--- backend/app/Person.php
+++ backend/app/Person.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    public function getAgeAttribute()
+    {
+        return $this->dob->diffInYears(\Carbon\Carbon::now());
+    }
 }
